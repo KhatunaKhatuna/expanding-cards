fa01f90a4bdaf1df38e40828656f794b21e15922
KhatunaKhatuna <kh.chitishvili@gmail.com>
2025-07-15 03:46:47 +0400

    feat: implement JavaScript version 2 to expand selected card docs:correct typing mistake

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
+// Version 1
+
+// const cardEls = document.querySelectorAll(".card");
+
+// cardEls.forEach((card) => {
+//   card.addEventListener("click", () => {
+//     removeActiveClasses();
+// Add active to clicked card
+//     card.classList.add("active");
+//   });
+// });
+// Remove active from all cards
+// function removeActiveClasses() {
+//   cardEls.forEach((card) => {
+//     card.classList.remove("active");
+//   });
+// }
+
+// Version 2
+
 const cardEls = document.querySelectorAll(".card");
+let activeCard = cardEls[0];
+
+// Set the first card active on page load
+if (activeCard) {
+  activeCard.classList.add("active");
+}
 
 cardEls.forEach((card) => {
   card.addEventListener("click", () => {
-    removeActiveClasses();
-    card.classList.add("active");
+    if (activeCard === card) {
+      activeCard = null;
+      return;
+    } else {
+      // Remove active from previous active card
+      if (activeCard) activeCard.classList.remove("active");
+      // Add active to clicked card
+      card.classList.add("active");
+      activeCard = card;
+    }
   });
 });
-
-function removeActiveClasses() {
-  cardEls.forEach((card) => {
-    card.classList.remove("active");
-  });
-}
